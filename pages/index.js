b116955528d2763da7d90c94008c0010cf2911b6
Jacob Mishkin <jacobmishkin@Jacobs-MacBook-Pro.local>
2021-08-24 16:12:46 -0500

    eslint setup

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,20 @@
-import Head from "next/head";
+import Head from 'next/head';
 
-const Index = () => {
-  return (
-    <div>
-      <Head>
-        <title>Foxtrot</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://use.typekit.net/zdz0zoa.css" />
-      </Head>
-      <main>
-        <h1>Welcome to the Foxtrot front-end coding project</h1>
-        <p>
-          Please take a look at the README before staring and feel free to reach
-          out with any questions or concerns!
-        </p>
-      </main>
-    </div>
-  );
-};
+const Index = () => (
+  <div>
+    <Head>
+      <title>Foxtrot</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://use.typekit.net/zdz0zoa.css" />
+    </Head>
+    <main>
+      <h1>Welcome to the Foxtrot front-end coding project</h1>
+      <p>
+        Please take a look at the README before staring and feel free to reach
+        out with any questions or concerns!
+      </p>
+    </main>
+  </div>
+);
 
 export default Index;
