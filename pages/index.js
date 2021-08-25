5acf7daf412a76948dee664caf024b3b98a746a3
Jacob Mishkin <jacobmishkin@Jacobs-MacBook-Pro.local>
2021-08-24 20:47:45 -0500

    base styles for header and svg icons

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Head from 'next/head';
+import Header from '../components/Header';
 
 const Index = () => (
   <div>
+    <Head>
+      <title>Foxtrot</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://use.typekit.net/zdz0zoa.css" />
+    </Head>
     <main>
-      <h1>Welcome to the Foxtrot front-end coding project</h1>
+      <Header />
       <p>
         Please take a look at the README before staring and feel free to reach
         out with any questions or concerns!
